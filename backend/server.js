fed0e97b2989c38d6619673f93ec8381af19a48c
Krizen Knoz <krizenknoz@gmail.com>
2023-03-03 18:51:14 +0530

    feat: Adding support for serverless hosting in backend

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,30 @@ const mongoose = require("mongoose");
 const app = require("./src/app");
 
 dotenv.config();
-
-mongoose
-  .connect(process.env.MONGO_DB_URI || "127.0.0.1")
-  .then(console.log("[Booting]: Connected to DB Sucessfully!"))
-  .catch((err) => console.log(err));
-
 const PORT = process.env.PORT || 3120;
 
-app.listen(PORT, () =>
-  console.log("[Booting]: Server process has been initiated")
-);
+// mongoose
+//   .connect(process.env.MONGO_DB_URI || "127.0.0.1")
+//   .then(console.log("[Booting]: Connected to DB Sucessfully!"))
+//   .catch((err) => console.log(err));
+
+// app.listen(PORT, () =>
+//   console.log("[Booting]: Server process has been initiated")
+// );
+
+// * For Serverless hosting
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("listening for requests");
+  });
+});
